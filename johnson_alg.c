22c20519553e5003d12e3a9517b1e598100db724
Gocode277 <137083457+Gocode277@users.noreply.github.com>
2023-07-01 12:29:40 +0530

    johnson_alg.c

--- johnson_alg.c
+++ johnson_alg.c
@@ -7,7 +7,8 @@ int dir[MAXN];
 
 void printPermutation(int n)
 {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         printf("%d ", p[i]);
     }
     printf("\n");
@@ -22,33 +23,42 @@ void generatePermutations(int n)
 {
     printPermutation(n);
     int mobile, mobileIndex;
-    while (1) {
+    while (1) 
+    {
         mobile = 0;
         mobileIndex = -1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+            {
             if ((dir[i] == -1 && i > 0 && p[i] > p[i - 1]) ||
-                (dir[i] == 1 && i < n - 1 && p[i] > p[i + 1])) {
-                if (p[i] > mobile) {
+                (dir[i] == 1 && i < n - 1 && p[i] > p[i + 1])) 
+            {
+                if (p[i] > mobile)
+                {
                     mobile = p[i];
                     mobileIndex = i;
                 }
             }
         }
-        if (mobileIndex == -1) {
+        if (mobileIndex == -1)
+        {
             break;
         }
-        if (dir[mobileIndex] == -1) {
+        if (dir[mobileIndex] == -1) 
+        {
             swap(&p[mobileIndex], &p[mobileIndex - 1]);
             swap(&dir[mobileIndex], &dir[mobileIndex - 1]);
             mobileIndex--;
         }
-        else if (dir[mobileIndex] == 1) {
+        else if (dir[mobileIndex] == 1) 
+        {
             swap(&p[mobileIndex], &p[mobileIndex + 1]);
             swap(&dir[mobileIndex], &dir[mobileIndex + 1]);
             mobileIndex++;
         }
-        for (int i = 0; i < n; i++) {
-            if (p[i] > mobile) {
+        for (int i = 0; i < n; i++) 
+        {
+            if (p[i] > mobile) 
+            {
                 dir[i] = -dir[i];
             }
         }
@@ -60,11 +70,13 @@ int main()
     int n;
     printf("Enter the value of n (maximum %d): ", MAXN);
     scanf("%d", &n);
-    if (n <= 0 || n > MAXN) {
+    if (n <= 0 || n > MAXN)
+    {
         printf("Invalid value of n!\n");
         return 0;
     }
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         p[i] = i + 1;
         dir[i] = -1;
     }
